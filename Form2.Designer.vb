c7660c95ebb5f81454b1e7a7174ea01992916a09
Phattiya <phattiya.ra@kw.ac.th>
2018-12-04 11:48:23 +0700

    041261

---
+++ Form2.Designer.vb
@@ -0,0 +1,264 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtSales1 = New System.Windows.Forms.TextBox()
+        Me.txtSales2 = New System.Windows.Forms.TextBox()
+        Me.lblSales1 = New System.Windows.Forms.Label()
+        Me.lblSales2 = New System.Windows.Forms.Label()
+        Me.lblSumSales = New System.Windows.Forms.Label()
+        Me.lblCom1 = New System.Windows.Forms.Label()
+        Me.lblCom2 = New System.Windows.Forms.Label()
+        Me.lblSumCom = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtSales1
+        '
+        Me.txtSales1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.txtSales1.Location = New System.Drawing.Point(205, 51)
+        Me.txtSales1.Name = "txtSales1"
+        Me.txtSales1.Size = New System.Drawing.Size(100, 22)
+        Me.txtSales1.TabIndex = 0
+        '
+        'txtSales2
+        '
+        Me.txtSales2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.txtSales2.Location = New System.Drawing.Point(205, 103)
+        Me.txtSales2.Name = "txtSales2"
+        Me.txtSales2.Size = New System.Drawing.Size(100, 22)
+        Me.txtSales2.TabIndex = 1
+        '
+        'lblSales1
+        '
+        Me.lblSales1.BackColor = System.Drawing.Color.DarkSalmon
+        Me.lblSales1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblSales1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.lblSales1.Location = New System.Drawing.Point(202, 141)
+        Me.lblSales1.Name = "lblSales1"
+        Me.lblSales1.Size = New System.Drawing.Size(100, 23)
+        Me.lblSales1.TabIndex = 2
+        Me.lblSales1.Text = "                              "
+        '
+        'lblSales2
+        '
+        Me.lblSales2.BackColor = System.Drawing.Color.DarkSalmon
+        Me.lblSales2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblSales2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.lblSales2.Location = New System.Drawing.Point(202, 177)
+        Me.lblSales2.Name = "lblSales2"
+        Me.lblSales2.Size = New System.Drawing.Size(100, 23)
+        Me.lblSales2.TabIndex = 3
+        Me.lblSales2.Text = "                              "
+        '
+        'lblSumSales
+        '
+        Me.lblSumSales.BackColor = System.Drawing.Color.DarkSalmon
+        Me.lblSumSales.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblSumSales.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.lblSumSales.Location = New System.Drawing.Point(202, 212)
+        Me.lblSumSales.Name = "lblSumSales"
+        Me.lblSumSales.Size = New System.Drawing.Size(100, 23)
+        Me.lblSumSales.TabIndex = 4
+        Me.lblSumSales.Text = "                              "
+        '
+        'lblCom1
+        '
+        Me.lblCom1.BackColor = System.Drawing.Color.DarkSalmon
+        Me.lblCom1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblCom1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.lblCom1.Location = New System.Drawing.Point(202, 251)
+        Me.lblCom1.Name = "lblCom1"
+        Me.lblCom1.Size = New System.Drawing.Size(100, 22)
+        Me.lblCom1.TabIndex = 5
+        Me.lblCom1.Text = "                              "
+        '
+        'lblCom2
+        '
+        Me.lblCom2.BackColor = System.Drawing.Color.DarkSalmon
+        Me.lblCom2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblCom2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.lblCom2.Location = New System.Drawing.Point(202, 284)
+        Me.lblCom2.Name = "lblCom2"
+        Me.lblCom2.Size = New System.Drawing.Size(100, 23)
+        Me.lblCom2.TabIndex = 6
+        Me.lblCom2.Text = "                              "
+        '
+        'lblSumCom
+        '
+        Me.lblSumCom.BackColor = System.Drawing.Color.DarkSalmon
+        Me.lblSumCom.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblSumCom.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.lblSumCom.Location = New System.Drawing.Point(202, 315)
+        Me.lblSumCom.Name = "lblSumCom"
+        Me.lblSumCom.Size = New System.Drawing.Size(100, 23)
+        Me.lblSumCom.TabIndex = 7
+        Me.lblSumCom.Text = "                              "
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.YellowGreen
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Button1.Location = New System.Drawing.Point(319, 305)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 33)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "แสดงค่า"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label7.Location = New System.Drawing.Point(29, 57)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(158, 16)
+        Me.Label7.TabIndex = 9
+        Me.Label7.Text = "Computer Acer Veriton x5"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label8.Location = New System.Drawing.Point(29, 103)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(123, 16)
+        Me.Label8.TabIndex = 10
+        Me.Label8.Text = "Printer HP M529DN"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label9.Location = New System.Drawing.Point(29, 148)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(94, 16)
+        Me.Label9.TabIndex = 11
+        Me.Label9.Text = "ยอดขาบ Sales 1"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label10.Location = New System.Drawing.Point(30, 178)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(93, 16)
+        Me.Label10.TabIndex = 12
+        Me.Label10.Text = "ยอดขาย Sales 2"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label11.Location = New System.Drawing.Point(29, 213)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(64, 16)
+        Me.Label11.TabIndex = 13
+        Me.Label11.Text = "รวมยอดขาย"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label12.Location = New System.Drawing.Point(29, 252)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(135, 16)
+        Me.Label12.TabIndex = 14
+        Me.Label12.Text = "Commission 1 ร้อยละ 5"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label13.Location = New System.Drawing.Point(29, 285)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(142, 16)
+        Me.Label13.TabIndex = 15
+        Me.Label13.Text = "Commission 2 ร้อยละ 10"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label14.Location = New System.Drawing.Point(29, 316)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(104, 16)
+        Me.Label14.TabIndex = 16
+        Me.Label14.Text = "รวม Commission"
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.AntiqueWhite
+        Me.ClientSize = New System.Drawing.Size(406, 364)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.lblSumCom)
+        Me.Controls.Add(Me.lblCom2)
+        Me.Controls.Add(Me.lblCom1)
+        Me.Controls.Add(Me.lblSumSales)
+        Me.Controls.Add(Me.lblSales2)
+        Me.Controls.Add(Me.lblSales1)
+        Me.Controls.Add(Me.txtSales2)
+        Me.Controls.Add(Me.txtSales1)
+        Me.Name = "Form2"
+        Me.Text = "Form2"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtSales1 As TextBox
+    Friend WithEvents txtSales2 As TextBox
+    Friend WithEvents lblSales1 As Label
+    Friend WithEvents lblSales2 As Label
+    Friend WithEvents lblSumSales As Label
+    Friend WithEvents lblCom1 As Label
+    Friend WithEvents lblCom2 As Label
+    Friend WithEvents lblSumCom As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+End Class
---
+++ Form2.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form2
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Sales1 As Double
+        Dim Sales2 As Double
+        Dim SumSales As Double
+        Dim Commission1 As Double
+        Dim Commission2 As Double
+        Dim SumCommission As Double
+
+        Sales1 = txtSales1.Text
+        Sales2 = txtSales2.Text
+        SumSales = Sales1 + Sales2
+        Commission1 = Sales1 * 0.05
+        Commission2 = Sales2 * 0.1
+        SumCommission = Commission1 + Commission2
+
+        lblCom1.Text = Commission1
+        lblCom2.Text = Commission2
+        lblSumCom.Text = SumCommission
+        lblSumSales.Text = SumSales
+        lblSales1.Text = Sales1
+        lblSales2.Text = Sales2
+
+
+    End Sub
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
